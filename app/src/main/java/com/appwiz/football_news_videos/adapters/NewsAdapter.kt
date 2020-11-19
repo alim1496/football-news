f0d27cd1989607f1ccdc95da59d9bf69873abd85
alim1496 <alim1496@gmail.com>
2020-11-19 18:50:17 +0600

    start for news site

--- app/src/main/java/com/appwiz/football_news_videos/adapters/NewsAdapter.kt
+++ app/src/main/java/com/appwiz/football_news_videos/adapters/NewsAdapter.kt
@@ -8,24 +8,17 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.appwiz.football_news_videos.R
 import com.appwiz.football_news_videos.models.News
+import com.appwiz.football_news_videos.models.NewsSite
 import com.squareup.picasso.Picasso
 
-class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
-
-    private var newslist:MutableList<News> = ArrayList()
+class NewsAdapter(private var newslist: List<NewsSite>) : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
 
     class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bindview(news:News) {
+        fun bindview(news:NewsSite) {
             val title:TextView = itemView.findViewById(R.id.title)
-            val source:TextView = itemView.findViewById(R.id.source)
-            val time:TextView = itemView.findViewById(R.id.time)
-            val desc:TextView = itemView.findViewById(R.id.desc)
-            val cover:ImageView = itemView.findViewById(R.id.cover)
-            title.text = news.title
-            source.text = news.source.name
-            time.text = news.time
-            desc.text = news.description
-            Picasso.get().load(news.image).placeholder(R.drawable.ic_image_ph).into(cover)
+            val logo:ImageView = itemView.findViewById(R.id.logo)
+            title.text = news.name
+            Picasso.get().load(R.drawable.ic_bbc).into(logo)
         }
     }
 
@@ -40,16 +33,5 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         val news = newslist.get(position)
         holder.bindview(news)
-    }
-
-    fun reload(newslist:List<News>) {
-        this.newslist.clear()
-        this.newslist.addAll(newslist)
-        notifyDataSetChanged()
-    }
-
-    fun append(newslist:List<News>) {
-        this.newslist.addAll(newslist)
-        notifyItemRangeInserted(this.newslist.size - newslist.size, newslist.size)
     }
 }
